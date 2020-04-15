0cc458c708e632a8e913393b89c87d432c93e503
linjw <10126121@qq.com>
2020-04-15 13:49:21 +0800

    no message

--- customize.sh
+++ customize.sh
@@ -7,3 +7,7 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 修改路由器名称
+sed -i 's/OpenWrt/LinjwRouter/g' package/base-files/files/bin/config_generate
+# 修改IP
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
