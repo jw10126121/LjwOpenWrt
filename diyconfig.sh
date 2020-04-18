9f14eb2f81c637b56b78c83bb8097ccf39536add
linjw <10126121@qq.com>
2020-04-18 18:50:39 +0800

    修改ip地址为192.168.31.1

--- diyconfig.sh
+++ diyconfig.sh
@@ -12,7 +12,7 @@ package_root='package'
 sed -i 's/OpenWrt/LinjwRouter/g' $package_root/base-files/files/bin/config_generate
 
 # 修改IP
-sed -i 's/192.168.1.1/192.168.1.1/g' $package_root/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.1/g' $package_root/base-files/files/bin/config_generate
 #sed -i 's/192.168.\$(/192.168.\$(/g' $package_root/base-files/files/bin/config_generate
 
 # 修改wifi信息
