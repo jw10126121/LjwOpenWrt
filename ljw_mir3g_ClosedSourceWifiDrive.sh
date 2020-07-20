f2d9940cc7bc8e2f66ea06a0a5aa76fdd81ab5cb
linjw <10126121@qq.com>
2020-07-21 00:05:40 +0800

    MiR3G

---
+++ ljw_mir3g_ClosedSourceWifiDrive.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+#
+#=================================================
+
+
+config_file=".config"
+
+grep -l "CONFIG_PACKAGE_kmod-mt7603=y" $config_file
+if [ ! $? -eq 0 ]; then 
+	sed -i "/CONFIG_PACKAGE_kmod-mt7603=/d" $config_file
+fi
+
+
+# 修改路由器名称
+# sed -i 's/OpenWrt/LinjwRouter/g' $package_root/base-files/files/bin/config_generate
+
+## 修改IP
+## sed -i 's/192.168.1.1/192.168.31.1/g' $package_root/base-files/files/bin/config_generate
+
+# 修改wifi信息
+
+# 修改wifi国家
+# sed -i 's/set wireless.radio\${devidx}.type=mac80211/set wireless.radio\${devidx}.type=mac80211 \n\t\t\t set wireless.radio\${devidx}.country=\"CN\"/g' $package_root/kernel/mac80211/files/lib/wifi/mac80211.sh
+## 修改2.4G wifi信道
+## sed -i 's/channel=\"11\"/channel=\"1\"/g' $package_root/kernel/mac80211/files/lib/wifi/mac80211.sh
+## 修改5G wifi信道
+## sed -i 's/channel=\"36\"/channel=\"153\"/g' $package_root/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 修改wifi名
+# sed -i 's/set wireless.default_radio\${devidx}.ssid=OpenWrt/set wireless.default_radio\${devidx}.ssid=ljwAP/g' $package_root/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 修改wifi密码
+# sed -i 's/set wireless.default_radio\${devidx}.encryption=none/set wireless.default_radio\${devidx}.encryption=psk-mixed \n\t\t\t set wireless.default_radio\${devidx}.key=12356789/g' $package_root/kernel/mac80211/files/lib/wifi/mac80211.sh
+
