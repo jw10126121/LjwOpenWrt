17dee975723d9886efcc74a4ff55d170398acf50
linjw <10126121@qq.com>
2025-01-22 17:33:45 +0800

    修改报错问题

--- Scripts/diy_after_defconfig.sh
+++ Scripts/diy_after_defconfig.sh
@@ -74,31 +74,36 @@ fi
 
 choose_type_homeproxy=$(grep -m 1 "^CONFIG_PACKAGE_luci-app-homeproxy=" ./.config | awk -F'=' '{print $2}' | tr -d '"')
 # homeproxy_DIR=$(find ./package ./feeds/luci/ ./feeds/packages/ -maxdepth 3 -type d -iname "luci-app-homeproxy" -prune)
-homeproxy_DIR=$(readlink -f $(find ./package ./feeds/luci/ ./feeds/packages/ -maxdepth 3 -type d -iname "luci-app-homeproxy" -prune))
+app_homeproxy_dir=$(find ./package ./feeds/luci/ ./feeds/packages/ -maxdepth 3 -type d -iname "luci-app-homeproxy" -prune)
+if [ -n "${choose_type_homeproxy}" ] && [ -d "${app_homeproxy_dir}" ]; then
 
-# 预置HomeProxy数据
-if [ -n "${choose_type_homeproxy}" ] && [ -d "${homeproxy_DIR}" ]; then
-    HP_RULES="${homeproxy_DIR}/root/etc/homeproxy/my_surge"
-    HP_PATCH="${homeproxy_DIR}/root/etc/homeproxy"
+    homeproxy_DIR=$(readlink -f "${app_homeproxy_dir}")
 
-    chmod +x $HP_PATCH/scripts/*
-    rm -rf $HP_PATCH/resources/*
-    [ -d "${HP_RULES}" ] && rm -fr "${HP_RULES}"
-    mkdir -p "${HP_RULES}"
+    # 预置HomeProxy数据
+    if [ -d "${homeproxy_DIR}" ]; then
+        HP_RULES="${homeproxy_DIR}/root/etc/homeproxy/my_surge"
+        HP_PATCH="${homeproxy_DIR}/root/etc/homeproxy"
 
-    git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" "${HP_RULES}"
-    cd "${HP_RULES}" && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
-    
-    echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
-    awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
-    sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
+        chmod +x $HP_PATCH/scripts/*
+        rm -rf $HP_PATCH/resources/*
+        [ -d "${HP_RULES}" ] && rm -fr "${HP_RULES}"
+        mkdir -p "${HP_RULES}"
 
-    mv -f ${HP_RULES}/{china_*,gfw_list}.{ver,txt} ${HP_PATCH}/resources/
+        git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" "${HP_RULES}"
+        cd "${HP_RULES}" && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
+        
+        echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
+        awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
+        sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
 
-    cd .. && rm -rf "${HP_RULES}"
+        mv -f ${HP_RULES}/{china_*,gfw_list}.{ver,txt} ${HP_PATCH}/resources/
 
-    echo "【LinInfo】homeproxy date has been updated!"
+        cd .. && rm -rf "${HP_RULES}"
+
+        echo "【LinInfo】homeproxy date has been updated!"
+    fi 
 fi
+
 
 
 
