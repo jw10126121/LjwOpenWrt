53678bdd535659e4c090fe41b1fbc6a568418fb8
linjw <10126121@qq.com>
2025-07-04 17:00:26 +0800

    去掉sqm_scripts_nss，因为m2用不了，用了就会出现宽带被T的情况，会一直拨号连不上

--- Scripts/diy_feeds.sh
+++ Scripts/diy_feeds.sh
@@ -14,9 +14,9 @@ if [[ -z $version_firewall ]]; then
 fi
 
 sed -i "s/#src-git helloworld/src-git helloworld/g" $feed_config_name
-if ! grep -q '^[^#]*sqm-scripts-nss\.git$' "$feed_config_name"; then
-	# sed -i '$a src-git sqm_scripts_nss https://github.com/qosmio/sqm-scripts-nss.git' $feed_config_name
-fi
+# if ! grep -q '^[^#]*sqm-scripts-nss\.git$' "$feed_config_name"; then
+# 	sed -i '$a src-git sqm_scripts_nss https://github.com/qosmio/sqm-scripts-nss.git' $feed_config_name
+# fi
 
 if [[ $version_firewall == "4" ]]; then
 	# rm -rf ./package/qca
