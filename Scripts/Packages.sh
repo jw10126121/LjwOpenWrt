d427b9dee64128669776d6047fcf75a84547906d
linjw <10126121@qq.com>
2026-05-13 22:16:03 +0800

    easytier限制分支为releases/v2.6.4

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -295,7 +295,9 @@ apply_common_package_overrides() {
     UPDATE_PACKAGE "luci-app-wechatpush" "tty228/luci-app-wechatpush" "master"
     UPDATE_PACKAGE "luci-app-pushbot" "zzsj0928/luci-app-pushbot" "master"
 
-    update_package_list "luci-app-easytier easytier" "EasyTier/luci-app-easytier" "main"
+    # update_package_list "luci-app-easytier easytier" "EasyTier/luci-app-easytier" "main"
+    update_package_list "luci-app-easytier easytier" "EasyTier/luci-app-easytier" "releases/v2.6.4"
+    
 
     UPDATE_PACKAGE "luci-app-bandix" "timsaya/luci-app-bandix" "main"
     UPDATE_PACKAGE "openwrt-bandix" "timsaya/openwrt-bandix" "main"
