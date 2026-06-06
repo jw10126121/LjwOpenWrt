6e520ae2146c1f3db959a3ff99069944da6d1184
jw10126121 <10126121@qq.com>
2026-06-06 13:03:48 +0800

    Comment out configuration and preload functions

--- Scripts/diy_after_defconfig.sh
+++ Scripts/diy_after_defconfig.sh
@@ -176,9 +176,9 @@ preload_homeproxy_resources() {
 }
 
 cd "${openwrt_workdir}"
-configure_ecm_accel_delay_fix
+#configure_ecm_accel_delay_fix
 # preload_openclash_meta_core
-preload_homeproxy_resources
+#preload_homeproxy_resources
 
 
 
